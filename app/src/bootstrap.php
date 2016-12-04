64e680d1b287a87d1615fb23960c083eaf9785a4
Isaac Bowen <irbowen@umich.edu>
2016-12-03 20:30:19 -0500

    got things working!

--- app/src/bootstrap.php
+++ app/src/bootstrap.php
@@ -1,13 +1,37 @@
 <?php declare(strict_types = 1);
-
 require_once __DIR__ . '/../vendor/autoload.php';
+
+$servername = "localhost";
 
 $klein = new \Klein\Klein();
 
 $klein->respond('GET', '/', function () {
-  print_r($_REQUEST);
-  return 'Hello World!';
+
+  $dummy = "test";
+  echo $dummy;
+
+  /*
+  $conn = new mysqli($servername, $username, $password, $dbname);
+
+  $sql_query = "select * from lifts";
+  $result = $conn->query($sql_query);
+
+  if ($result->num_rows > 0) {
+      echo "<table><tr><th>ID</th><th>Name</th></tr>";
+      while($row = $result->fetch_assoc()) {
+          echo "<tr><td>".$row["id"]."</td><td>".$row["firstname"]." ".$row["lastname"]."</td></tr>";
+      }
+      echo "</table>";
+  } else {
+      echo "0 results";
+  }
+  $conn->close();
+  */
+
 });
+
+
+
 $klein->respond('GET', '/hello-world', function () {
   print_r($_REQUEST);
   return 'Hello World!';
