f3eca02176520bb9e46a4b5faefc7ec2781585af
Isaac Bowen <irbowen@umich.edu>
2016-12-07 21:05:13 -0500

    doing some php and pdo

--- app/src/bootstrap.php
+++ app/src/bootstrap.php
@@ -1,40 +1,41 @@
-<?php declare(strict_types = 1);
+<?php
+declare(strict_types = 1);
 require_once __DIR__ . '/../vendor/autoload.php';
 
-$servername = "localhost";
+require_once __DIR__ . '/database.php';
 
 $klein = new \Klein\Klein();
 
 $klein->respond('GET', '/', function () {
 
-  $dummy = "test";
-  echo $dummy;
+  $servername = "localhost";
+  $username = "root";
+  $password = "root";
+  $dbname = "weight";
 
-  /*
   $conn = new mysqli($servername, $username, $password, $dbname);
 
   $sql_query = "select * from lifts";
   $result = $conn->query($sql_query);
 
+  echo "<table>";
   if ($result->num_rows > 0) {
-      echo "<table><tr><th>ID</th><th>Name</th></tr>";
-      while($row = $result->fetch_assoc()) {
-          echo "<tr><td>".$row["id"]."</td><td>".$row["firstname"]." ".$row["lastname"]."</td></tr>";
-      }
-      echo "</table>";
+    echo "<tr><th>Name</th><th>Description</th></tr>";
+    while ($row = $result->fetch_assoc()) {
+      echo "<tr><td>".$row["name"]."</td><td>".$row["description"]."</td></tr>";
+    }
+    echo "</table>";
   } else {
-      echo "0 results";
+    echo "0 results";
   }
   $conn->close();
-  */
 
 });
 
 
 
 $klein->respond('GET', '/hello-world', function () {
-  print_r($_REQUEST);
-  return 'Hello World!';
+  query_db();
 });
 
 $klein->dispatch();
